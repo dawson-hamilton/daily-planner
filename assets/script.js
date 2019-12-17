576735002b46cf0612899b9890b8166f2960dfca
PRISON.MIKE <dawsondhamilton@gmail.com>
2019-12-17 13:18:41 -0700

    moving to desktop

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,15 @@
+var saveEl = $("#saveBtn");
+
+var input = [];
+
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 setInterval(function () {
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 }, 1000);
+
+function storeInput() {
+    localStorage.setItem("input", JSON.stringify(input));
+}
+
+$("#saveBtn").on("click", storeInput());
