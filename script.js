859f11955daba71f0d66d3216cb14d0918e989e1
PRISON.MIKE <dawsondhamilton@gmail.com>
2019-12-18 18:46:35 -0700

    finishing everything and seriously condensing the code to look much cleaner

---
+++ script.js
@@ -0,0 +1,34 @@
+var input = [];
+var timeFormat = 'HH:mm:ss'
+var currentHour = moment().format('HH');
+
+//put time in the beginning
+$("#currentDay").text(moment().format('MMMM Do YYYY,HH:mm:ss'));
+
+//set the time on the top and update every second
+setInterval(function () {
+    $("#currentDay").text(moment().format('MMMM Do YYYY,HH:mm:ss'));
+}, 1000);
+
+function hoursUpdater() {
+    $(".time-block").each(function () {
+        var blockKey = $(this).attr("id");
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        var blockDisplay = localStorage.getItem(blockKey);
+        $(this).children("textarea").text(blockDisplay);
+
+        if (currentHour === blockHour) {
+            $(this).children("textarea").css("background-color", "red");
+        } else if (currentHour < blockHour) {
+            $(this).children("textarea").css("background-color", "green");
+        } else if (currentHour > blockHour) {
+            $(this).children("textarea").css("background-color", "lightGrey");
+        }
+    });
+
+    $(".saveBtn").on("click", function () {
+        localStorage.setItem($(this).parent().attr("id"), $(this).siblings("textarea").val().trim());
+    });
+}
+
+hoursUpdater();
